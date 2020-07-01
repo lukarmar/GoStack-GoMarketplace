3f7e5db033e2d3f0a51e1a146e2d1a2e393e1ec9
luka rocha <luka.rmar@gmail.com>
2020-07-01 17:26:59 -0300

    challenge finished

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -39,23 +39,25 @@ const Cart: React.FC = () => {
   const { increment, decrement, products } = useCart();
 
   function handleIncrement(id: string): void {
-    // TODO
+    increment(id);
   }
 
   function handleDecrement(id: string): void {
-    // TODO
+    decrement(id);
   }
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
-
-    return formatValue(0);
+    return formatValue(
+      products.reduce((total, item) => {
+        return total + item.price * item.quantity;
+      }, 0),
+    );
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
-
-    return 0;
+    return products.reduce((total, item) => {
+      return total + item.quantity;
+    }, 0);
   }, [products]);
 
   return (
